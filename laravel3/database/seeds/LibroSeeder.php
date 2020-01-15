0d5c0aabea170696a826c4a2b838a43589c656d2
Santzu-159 <santzura.insomniac@gmail.com>
2020-01-15 13:08:31 +0100

    laravel3

---
+++ laravel3/database/seeds/LibroSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Libro;
+
+class LibroSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //Primero borramos por si ejecutamos el seed varias veces no rellenar la tabla cada vez
+        DB::table('libros')->truncate(); //vacia la tabla
+        factory(Libro::class,20)->create(); // creo 10 registros
+    }
+}
